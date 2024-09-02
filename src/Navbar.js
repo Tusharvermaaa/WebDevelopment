fb076b4d09fc469be429aec9cbd1b4a3c0cbea9f
Tusharvermaaa <Tusharverma8433@gmail.com>
2024-09-02 23:57:17 +0530

    adding firsttest  component Navbar

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+
+
+function Navbar(){
+
+    return(
+
+        <><h1> this is sample code tested </h1><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat possimus ratione, ex laborum modi atque error quibusdam molestias asperiores debitis!</p></>
+    );
+}
+
+
+
+export default Navbar;
